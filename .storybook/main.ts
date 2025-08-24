c72c6ab1a9838eaa4712340d0fb9ce82afcc4874
Gursewak Mehra <gursewakmehra189@gmail.com>
2025-08-24 13:05:05 +0530

    add storybook link

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,22 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-docs",
     "@storybook/addon-onboarding"
   ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
-  "staticDirs": [
-    "..\\public"
+  staticDirs: [
+    "../public"  // Forward slash use kiya gaya hai
   ]
 };
+
 export default config;
